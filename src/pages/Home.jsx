2c63497ead42e49e17fbeee62d1511815f0501e3
Shohidul Arifin <shohidularifin@gmail.com>
2024-01-24 05:46:17 -0800

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Counter from "../components/Counter";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is home page</h1>
+      <Counter></Counter>
+    </div>
+  );
+};
+
+export default Home;
